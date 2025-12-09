2bdc8a9d93a2251ac83b874d40ddcb923af46040
sardeq <sadeqodeh57@gmail.com>
2025-12-09 21:53:09 +0300

    trying to fix stupid error

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <Ultralight/platform/Config.h>
 
 using namespace ultralight;
-namespace fs = std::filesystem;
 
 class ZyroBrowser : public AppListener,
                    public WindowListener,
@@ -19,49 +18,55 @@ class ZyroBrowser : public AppListener,
 
 public:
     ZyroBrowser() {
+        std::cout << "[1] Initializing Settings..." << std::endl;
         Config config;
-        
-        fs::path cwd = fs::current_path();
-        
-        std::string path_str = cwd.string();
-        if (path_str.back() != '/') {
-            path_str += "/";
-        }
-
-        path_str += "resources/";
-        
-        config.resource_path_prefix = path_str.c_str(); 
-        
-        std::cout << "Root path set to: " << path_str << std::endl;
-        std::cout << "Engine will look for: " << path_str << "resources/icudt67l.dat" << std::endl;
-
         Settings settings;
+        
+        // Use the GPU renderer by default, but we might need to disable it if this crashes
+        //config.use_gpu_driver = false; 
+
+        std::cout << "[2] Creating App..." << std::endl;
         app_ = App::Create(settings, config);
         
+        if (!app_) {
+            std::cerr << "[CRITICAL] App::Create returned null! Check your GPU drivers or assets." << std::endl;
+            exit(-1);
+        }
+
+        std::cout << "[3] Creating Window..." << std::endl;
         window_ = Window::Create(app_->main_monitor(), 800, 600, false, kWindowFlags_Titled);
         window_->SetTitle("Zyro Browser");
         
+        std::cout << "[4] Creating Overlay..." << std::endl;
         overlay_ = Overlay::Create(window_, 800, 600, 0, 0);
         
+        std::cout << "[5] Setting Listeners..." << std::endl;
         app_->set_listener(this);
         window_->set_listener(this);
         overlay_->view()->set_load_listener(this);
         
+        std::cout << "[6] Loading URL..." << std::endl;
         overlay_->view()->LoadURL("https://google.com");
+        
+        std::cout << "[7] Initialization Complete!" << std::endl;
     }
 
-    void Run() { app_->Run(); }
+    void Run() { 
+        std::cout << "[8] Entering Run Loop..." << std::endl;
+        app_->Run(); 
+    }
 
     virtual void OnUpdate() override {}
     virtual void OnClose(ultralight::Window* window) override { app_->Quit(); }
     virtual void OnResize(ultralight::Window* window, uint32_t width, uint32_t height) override {
-        overlay_->Resize(width, height);
+        if (overlay_) overlay_->Resize(width, height);
     }
     virtual void OnFinishLoading(ultralight::View* caller, uint64_t frame_id, bool is_main_frame, const String& url) override {}
     virtual void OnDOMReady(ultralight::View* caller, uint64_t frame_id, bool is_main_frame, const String& url) override {}
 };
 
 int main() {
+    std::cout << "--- STARTING ZYRO BROWSER ---" << std::endl;
     ZyroBrowser browser;
     browser.Run();
     return 0;
